e1a8c996030385a806a330f86d30b025c86ba3e0
Nuria <soriano.nuria@gmail.com>
2016-07-26 09:36:04 +0200

    adding grunt

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+module.exports = function(grunt) {
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-postcss');
+
+	grunt.initConfig({
+		sass: {
+			dist: {
+				options: {
+					style: 'compressed',
+				},
+				files: { 
+					'dist/style.css': 'sass/style.scss',
+				}
+			}
+		},
+		postcss: {
+            options: {
+                map: true,
+                processors: [
+                    require('autoprefixer')({
+                        browsers: ['last 2 versions']
+                    })
+                ]
+            },
+            dist: {
+                src: 'dist/*.css'
+            }
+        },
+		watch: {
+			options: {
+				livereload: true
+			},
+			css: {
+				files: 'sass/*.scss',
+				tasks: ['sass', 'postcss:dist']
+			},
+			// js: {
+			//     files: 'js/*.js',
+			//     tasks: ['uglify']
+			// }
+		},
+		uglify: {
+		    my_target: {
+			    files: {
+			        'dist/scripts.min.js': [
+			        	'js/*',
+			        ]
+			    }
+		    }
+		}
+	})
+
+	grunt.registerTask('default', ['watch'])
+};
