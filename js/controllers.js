137845e6def852dddc7e7a61ea12161e120ad507
Arturo Rubio <arturo.rubio.89.8@gmail.com>
2016-07-27 11:44:26 +0200

    home and age pages joined

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,21 @@
 angular.module( 'controllers', [ ] )
 	
-	.controller('homeController', function($scope){
+	.controller('homeController', function($scope, $http){
 
+		$scope.resultSearchedBeers = "";
+
+		var urlApiBeers = 'https://quiet-inlet-67115.herokuapp.com/api/search/beers?q=';
+		var imageNotFound = '../img/image-not-found.png';
+		$scope.imageNot = imageNotFound;
+		$scope.submit = function() {
+
+			var urlFinal = urlApiBeers + $scope.beerToSearch;
+			$http.get( urlFinal )
+				.then( function( dataBeerSearched ) {
+					$scope.resultSearchedBeers = dataBeerSearched.data;
+					console.log($scope.resultSearchedBeers);
+				})
+		}
 	})
 
 	.controller('ageController', function($scope, $localStorage) {
