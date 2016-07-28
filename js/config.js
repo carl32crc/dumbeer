ad0f5c36fcb329edd341f37a5f868fc02678d155
Nuria <soriano.nuria@gmail.com>
2016-07-28 12:05:01 +0200

    route templates

--- js/config.js
+++ js/config.js
@@ -5,23 +5,23 @@ angular.module( 'config', [] )
 			$routeProvider
 
 				.when('/',{
-					templateUrl: '/templates/home.html',
+					templateUrl: 'templates/home.html',
 					controller: 'homeController'
 				})
 				.when('/home',{
-					templateUrl: '/templates/home.html',
+					templateUrl: 'templates/home.html',
 					controller: 'homeController'
 				})
 				.when('/age',{
-					templateUrl: '/templates/age.html',
+					templateUrl: 'templates/age.html',
 					controller: 'ageController'
 				})
 				.when('/specifications/:ID',{
-					templateUrl: '/templates/spec.html',
+					templateUrl: 'templates/spec.html',
 					controller: 'specController'
 				})
 				.when('/myprofile',{
-					templateUrl: '/templates/profile.html',
+					templateUrl: 'templates/profile.html',
 					controller: 'checkInController'
 				})
 				//.otherwise({ redirectTo: '/' }); ;
