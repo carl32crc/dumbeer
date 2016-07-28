ccf777574b1ba40b593ebe93bc0d17c7dd8029ef
Arturo Rubio <arturo.rubio.89.8@gmail.com>
2016-07-28 14:55:46 +0200

    last searches done

--- js/controllers.js
+++ js/controllers.js
@@ -1,21 +1,37 @@
 angular.module( 'controllers', [ ] )
 	
-	.controller('homeController', function($scope, $http){
+	.controller('homeController', function($scope, $http, $localStorage){
 
 		$scope.resultSearchedBeers = "";
 
+    $scope.storage = $localStorage;
+    
 		var urlApiBeers = 'https://quiet-inlet-67115.herokuapp.com/api/search/beers?q=';
 		var imageNotFound = 'img/image-not-found.png';
 		$scope.imageNot = imageNotFound;
 		$scope.submit = function() {
 
+    if ($scope.storage.searches === undefined) {
+      $scope.storage.searches = []; }
+      $scope.storage.searches.push({ beer : $scope.beerToSearch, url : 'https://quiet-inlet-67115.herokuapp.com/api/search/beers?q='+$scope.beerToSearch })
+
+
 			var urlFinal = urlApiBeers + $scope.beerToSearch;
 			$http.get( urlFinal )
 				.then( function( dataBeerSearched ) {
 					$scope.resultSearchedBeers = dataBeerSearched.data;
-					console.log($scope.resultSearchedBeers);
+					// console.log($scope.resultSearchedBeers);
 				})
 		}
+
+    $scope.change = function() {
+      var url = $scope.selectSearcher;
+      $http.get( url )
+        .then( function( dataBeerSearched ) {
+          $scope.resultSearchedBeers = dataBeerSearched.data;
+          // console.log($scope.resultSearchedBeers);
+        })
+    }
 
     $scope.advOpt = false;
     $scope.showOpt = function() {
@@ -125,4 +141,5 @@ angular.module( 'controllers', [ ] )
           
         };
       })
+
 
